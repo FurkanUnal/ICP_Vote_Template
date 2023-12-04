ef94e04c587715989af71a687432be3bea9387a1
FurkanUnal <unal_furkan@outlook.com>
2023-12-04 08:58:33 +0300

    bugs fixed

--- src/final_project_backend/src/lib.rs
+++ src/final_project_backend/src/lib.rs
@@ -1,8 +1,6 @@
-use candid::bindings::javascript::value;
 use candid::{CandidType, Decode, Deserialize, Encode};
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
-use std::cell::Ref;
 use std::{borrow::Cow, cell::RefCell};
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
